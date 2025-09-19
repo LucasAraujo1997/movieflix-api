df0137201569499de916650a0ffd4b85d5f9fef2
Lucas da Silva Araújo <araujocreeds@gmail.com>
2025-09-19 20:04:32 -0300

    prisma generate and conect from db sucess

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const port = 3000;
 const app = express();
+const prisma = new PrismaClient();
 
-app.get("/movies", (req, res) => {
-  res.send("Listagem de filmes");
+app.get("/movies", async (req, res) => {
+  const movies = await prisma.movie.findMany();
+  res.json(movies);
 });
 
 app.listen(port, () => {
   console.log(
-    `Servidor em execução na porta ${port} ou http://localhost:3000 `
+    `Servidor em execução na porta ${port} ou http://localhost:3000/movies `
   );
 });
